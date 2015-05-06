9445f25dd7341def4c3f1ac911f8d4a24e0e9c44
ingridweck <ingridweck00@gmail.com>
2015-05-06 10:25:12 +0200

    contact front page

--- sites/default/themes/front/js/launcher.js
+++ sites/default/themes/front/js/launcher.js
@@ -11,9 +11,10 @@ jQuery(document).ready(function(){
 	//cycle2
 	jQuery('.diaporama').cycle({
         timeout:8000
-    }); 
-	
+    });
 
+    var formHeight=jQuery('#colorbox').height();
+    jQuery(".show_cb").colorbox({inline:true, width:"50%", height: "60%"});
 
 	var elementPosition = jQuery('.front-large-menu').offset();
 
@@ -22,8 +23,8 @@ jQuery(document).ready(function(){
           jQuery('.front-large-menu').css('position','fixed').css('top','0').css('width','100%').css('z-index','500').css('background-color', 'rgba(51, 51, 51, 0.1)').css('margin-left', '0').css('padding', '0');
           jQuery('.front-logo').css('width', '110px').css('padding', '15px 20px 15px 40px');
           jQuery('h1').css('display', 'none');
-          jQuery('.main-menu').css('padding-top', '0');
-          jQuery('.main-menu').css('margin-top', '10px');
+          jQuery('.main-menu').css('padding-top', '0').css('margin-top', '10px');
+
           jQuery('#block-locale-language').css('display', 'none');
           jQuery('.front-main-content').css('margin-top','790px');      
 	    } 
@@ -31,13 +32,24 @@ jQuery(document).ready(function(){
 	    else {
 	        jQuery('.front-large-menu').css('position','static').css('width','').css('background-color', 'rgba(51, 51, 51, 0.1)').css('margin', '').css('padding','');
 	        jQuery('.front-logo').css('width', '').css('padding', '').css('margin', '');
-	        jQuery('.main-menu').css('padding-top', '');
-	        jQuery('.main-menu').css('margin-top', '');
+	        jQuery('.main-menu').css('padding-top', '').css('margin-top', '');
 	        jQuery('h1').css('display', 'block');
 	        jQuery('#block-locale-language').css('display', 'block');
 	        jQuery('.front-main-content').css('margin-top','');
 	    }
-	}); 
+	});
+
+
+    jQuery(function mailReserve() {
+        var x = document.getElementById('superDate').value;
+        var y = document.getElementById('superDate1').value;
+        var z = document.getElementById('superSelect').value;
+        var text = 'Bonjour, je souhaite réserver    ...' + z + '...     du ...' + x + '...      au ...' + y  + '...' ;
+        document.getElementById('edit-submitted-texte').innerHTML = text ;
+        var res = 'Réservation';
+        document.getElementById('edit-submitted-sujet').value = res ;
+    });
+
 });
 
 // var screenWidth=jQuery(window).width() /2;
@@ -53,15 +65,14 @@ jQuery(function(d, s, id) {
 	js.src = "//connect.facebook.net/fr_FR/sdk.js.xfbml=1&version=v2.0";
 	fjs.parentNode.insertBefore(js, fjs);
 }
-
 	(document, 'script', 'facebook-jssdk'));
 
 // 
 
-var formHeight=jQuery('#colorbox').height();
-jQuery(".show_cb").colorbox({inline:true, width:"50%", height: "100%"});
 
-// });
+
+
+
 
 
 
--- sites/default/themes/front/templates/page--front.tpl.php
+++ sites/default/themes/front/templates/page--front.tpl.php
@@ -1,81 +1,84 @@
 <div class="front container">
-  <div class="container-2">
-    <!-- Navigation -->
-    <?php
+    <div class="container-2">
+        <!-- Navigation -->
+        <?php
 
         include ("main-menu.php");
-        include ("get-mail.php");
-    ?>
-    <div class="diaporama">
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2058.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2014.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2107.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2122.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2147.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2165.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2196.jpg" width="100%"/>
-      <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2242.jpg" width="100%"/>
-     </div>
-    <!-- main content-->
-    <div class="front-main-content">
-      
-      <div class="front-sub-main-content">
-        <div class="front-quot">
-        <?php
-        $block = block_load('views', 'quotidien-block');
-        $block = _block_get_renderable_array(_block_render_blocks(array($block)));
-        print drupal_render($block);
         ?>
+        <div class="diaporama">
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2058.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2014.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2107.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2122.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2147.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2165.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2196.jpg" width="100%"/>
+            <img class=" " src="<?= drupal_get_path('theme', 'front') ?>/img/3L5A2242.jpg" width="100%"/>
         </div>
-        <div class="block-front-reserver">
-          <div class="block-cadeau">
+        <!-- main content-->
+        <div class="front-main-content">
+
+            <div class="front-sub-main-content">
+                <div class="front-quot">
+                    <?php
+                    $block = block_load('views', 'quotidien-block');
+                    $block = _block_get_renderable_array(_block_render_blocks(array($block)));
+                    print drupal_render($block);
+                    ?>
+                </div>
+                <div class="block-front-reserver">
+                    <div class="block-cadeau">
+                        <?php
+                        $block = block_load('views', 'front_cadeau-block');
+                        $block = _block_get_renderable_array(_block_render_blocks(array($block)));
+                        print drupal_render($block);
+                        ?>
+                    </div>
+                    <div class="block-form">
+                        <form class="front-form-fake">
+                        <h2>Réservez votre séjour !</h2>
+                            <label>Date du séjour</label>
+                            <input name="date"  type="text"  id="superDate" value="17.05.2016"/>
+                            <label class="jusque">à</label>
+                            <input name="date1" type="text" id="superDate1" value="30.05.2016" />
+                            <label>Gîte</label>
+                            <select id="superSelect" name="gite">
+                                <option value="Le Gai Logis">Gai Logis (5 lits)</option>
+                                <option value="Les Capucins">Les Capucins (22 lits)</option>
+                                <option value="La Conciergerie">La Conciergerie (6 lits)</option>
+                                <option value="la Sphair">La Sphair (2 pers.)</option>
+                            </select>
+                            <a href="#colorbox" class="show_cb" onclick="mailReserve()">Reserver</a>
+                        </form>
+
+                        <div style="display: none;">
+                            <div id="colorbox">
+                                <div class="explication">
+                                    <p>La réservation n'est validée qu'après payement. Pour disposer des informations banquaires du domaine et consulter les disponibilités, merci de nous renvoyer ce formulaire de contact.</p>
+                                </div>
+
+                                <?php
+                                $block = block_load('views', 'webform-block');
+                                $block = _block_get_renderable_array(_block_render_blocks(array($block)));
+                                print drupal_render($block);
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="melting-pot">
             <?php
-            $block = block_load('views', 'front_cadeau-block');
+            $block = block_load('views', 'melting_pot2-block');
             $block = _block_get_renderable_array(_block_render_blocks(array($block)));
             print drupal_render($block);
             ?>
-          </div>
-          <div class="block-form">
-            <form action="get-mail.php" method="GET" class="front-form-fake">
-              <h2>Réservez votre séjour !</h2>
-              <label>Date du séjour</label>
-              <input name="date" type="date" class="datepicker" placeholder="JJ/MM/AAAA"/>
-              <label class="jusque">à</label>
-              <input name="jusque" type="date" class="datepicker"/>
-              <label>Gîte</label>
-                <select name="gite">
-                  <option value="gai_logis">Gai Logis (5 lits)</option>
-                  <option value="les_capucins">Les Capucins (22 lits)</option>
-                  <option value="la_conciergerie">La Conciergerie (6 lits)</option>
-                </select> 
-              <!--input type="button" value="Réserver" class="show_cb"-->
-              <a href="#colorbox" class="show_cb">Réserver</a>
-            </form>
-            <div style="display: none;">
-              <div id="colorbox">
-              <!--webform (js: oncklick afficher colorbox et display information in input)-->
-              <?php
-                $block = block_load('views', 'webform-block');
-                $block = _block_get_renderable_array(_block_render_blocks(array($block)));
-                print drupal_render($block);
-              ?>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-    <div class="melting-pot">
-      <?php
-          $block = block_load('views', 'melting_pot2-block');
-          $block = _block_get_renderable_array(_block_render_blocks(array($block)));
-          print drupal_render($block);
+
+        <!-- Page-footer -->
+        <?php
+        include ("footer.php");
         ?>
     </div>
-
-    <!-- Page-footer -->
-      <?php
-        include ("footer.php");
-      ?>
-  </div>
 </div>
